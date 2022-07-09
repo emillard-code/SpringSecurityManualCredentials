955e734615a1dabf1708e5ac96f117d791d284a6
Emerson Millard <emillard.code@gmail.com>
2022-07-08 22:25:39 -0700

    Initial commit

---
+++ src/main/java/com/project/SpringSecurityManualCredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityManualCredentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityManualCredentialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springsecuritymanualcredentials/SpringSecurityManualCredentialsApplicationTests.java
@@ -0,0 +1,8 @@
+package com.project.springsecuritymanualcredentials;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityManualCredentialsApplicationTests {
+
+}
